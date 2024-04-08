0a6f555991fb1805b4d0320955996c53b380bf8c
ExfilGithubTesting <134644117+ExfilGithubTesting@users.noreply.github.com>
2024-04-08 10:16:08 -0500

    Pushing files to trusted source

---
+++ Moved_Truested_main/ByOrder.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+
+namespace NUnit.Project
+{
+    public class ByOrder
+    {
+        public static bool Test1Called;
+        public static bool Test2ACalled;
+        public static bool Test2BCalled;
+        public static bool Test3Called;
+
+        [Test, Order(5)]
+        public void Test1()
+        {
+            Test3Called = true;
+
+            Assert.That(Test1Called);
+            Assert.That(Test2ACalled is false);
+            Assert.That(Test2BCalled);
+        }
+
+        [Test, Order(0)]
+        public void Test2B()
+        {
+            Test2BCalled = true;
+
+            Assert.That(Test1Called);
+            Assert.That(Test2ACalled is false);
+            Assert.That(Test3Called is false);
+        }
+
+        [Test]
+        public void Test2A()
+        {
+            Test2ACalled = true;
+
+            Assert.That(Test1Called);
+            Assert.That(Test2BCalled);
+            Assert.That(Test3Called);
+        }
+
+        [Test, Order(-5)]
+        public void Test3()
+        {
+            Test1Called = true;
+
+            Assert.That(Test2ACalled is false);
+            Assert.That(Test2BCalled is false);
+            Assert.That(Test3Called is false);
+        }
+    }
+}
